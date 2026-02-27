aa713c44d6153e3ddffa65d463a1a100eda2f8db
naveenkumar.gs_istee <naveenkumar.gs_istee@DESKTOP-79SOHNU>
2026-02-27 12:58:33 +0530

    intial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Form } from '../app/form/form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [Form],
+  template: `<app-form></app-form>`
 })
-export class App {
-  protected readonly title = signal('basic-form-app');
-}
+export class App {}
---
+++ src/app/form/form.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './form.html',
+  styleUrls: ['./form.css']
+})
+export class Form {
+
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+      alert('Form Submitted Successfully!');
+    }
+  }
+}
