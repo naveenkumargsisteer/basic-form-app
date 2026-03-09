9c1681c39b249faa089479062b598ae07f92b68f
naveenkumar.gs_istee <naveenkumar.gs_istee@DESKTOP-79SOHNU>
2026-03-09 14:59:14 +0530

    Login

--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -13,8 +13,11 @@ import { HttpClient } from '@angular/common/http';
 export class Form {
 
   userForm: FormGroup;
+  loginForm: FormGroup;
+  showLogin = false;
 
-  private apiUrl = 'http://localhost:8000/register';
+  private registerUrl = 'http://localhost:8000/register';
+  private loginUrl = 'http://localhost:8000/login';
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.userForm = this.fb.group({
@@ -22,22 +25,44 @@ export class Form {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  toggleLogin() {
+    this.showLogin = !this.showLogin;
   }
 
   onSubmit() {
     if (this.userForm.valid) {
-
-      this.http.post(this.apiUrl, this.userForm.value)
+      this.http.post(this.registerUrl, this.userForm.value)
         .subscribe({
           next: (response) => {
-            console.log(response);
+            console.log('registered', response);
             this.userForm.reset();
           },
           error: (error) => {
-            console.error(error); 
+            console.error(error);
           }
         });
+    }
+  }
 
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.http.post(this.loginUrl, this.loginForm.value)
+        .subscribe({
+          next: (response) => {
+            console.log('logged in', response);
+            this.loginForm.reset();
+          },
+          error: (error) => {
+            console.error(error);
+          }
+        });
     }
   }
 }
